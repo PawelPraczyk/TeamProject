9b026ed47a703af3ffdffe36cc9c379b407825ea
Paweł <praczykp2705@vp.pl>
2019-03-26 15:38:08 +0100

    Enable migrations

---
+++ HomeBudget/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace HomeBudget.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HomeBudget.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "HomeBudget.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(HomeBudget.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
