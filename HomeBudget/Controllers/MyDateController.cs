14f4d24b11db6190d6cd7a03ae89ce66dad41adb
Janek Cieszczyk <janek.cieszczyk@gmail.com>
2019-03-28 12:30:09 +0100

    Calendar v1
    simple  datepicker

---
+++ HomeBudget/Controllers/MyDateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeBudget.Controllers
+{
+    public class MyDateController : Controller
+    {
+        // GET: MyDate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
