2f97f662844e2d8313961b5781a79341fbb05130
Weronika <wlisowska7@gmail.com>
2019-03-31 20:39:53 +0200

    ogarniecie buttonow z glownej strony do dodawania operacji

--- HomeBudget/Migrations/201903302057316_UpdateOperations.cs
+++ HomeBudget/Migrations/201903302057316_UpdateOperations.cs
@@ -9,16 +9,16 @@ namespace HomeBudget.Migrations
         {
             DropForeignKey("dbo.Incomes", "CategoryId", "dbo.CategoryIncomes");
             DropIndex("dbo.Incomes", new[] { "CategoryId" });
-            AddColumn("dbo.Incomes", "CategoryIncome_Id", c => c.Byte(nullable: false));
-            CreateIndex("dbo.Incomes", "CategoryIncome_Id");
-            AddForeignKey("dbo.Incomes", "CategoryIncome_Id", "dbo.CategoryIncomes", "Id", cascadeDelete: true);
+            AddColumn("dbo.Incomes", "CategoryId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Incomes", "CategoryId");
+            AddForeignKey("dbo.Incomes", "CategoryId", "dbo.CategoryIncomes", "Id", cascadeDelete: true);
         }
 
         public override void Down()
         {
-            DropForeignKey("dbo.Incomes", "CategoryIncome_Id", "dbo.CategoryIncomes");
-            DropIndex("dbo.Incomes", new[] { "CategoryIncome_Id" });
-            DropColumn("dbo.Incomes", "CategoryIncome_Id");
+            DropForeignKey("dbo.Incomes", "CategoryId", "dbo.CategoryIncomes");
+            DropIndex("dbo.Incomes", new[] { "CategoryId" });
+            DropColumn("dbo.Incomes", "CategoryId");
             CreateIndex("dbo.Incomes", "CategoryId");
             AddForeignKey("dbo.Incomes", "CategoryId", "dbo.CategoryIncomes", "Id", cascadeDelete: true);
         }
