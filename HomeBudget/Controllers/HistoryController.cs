baba223d4be0138c17e1a5f001229a18d255e5d5
Paweł <praczykp2705@vp.pl>
2019-04-03 12:39:00 +0200

    Add search by category

--- HomeBudget/Controllers/HistoryController.cs
+++ HomeBudget/Controllers/HistoryController.cs
@@ -25,19 +25,19 @@ namespace HomeBudget.Controllers
 
             return View();
         }
-        public ActionResult Incomes( int? i)
+        public ActionResult Incomes(string search, int? i)
         {
             var incomesHistory = _context.Incomes
                 .Include(g => g.CategoryIncome)
-                .Include(g => g.User).ToList().ToPagedList(i ?? 1,10);
+                .Include(g => g.User).Where(x => x.CategoryIncome.Name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1,10);
 
             return View(incomesHistory);
         }
-        public ActionResult Outgoings(int? i)
+        public ActionResult Outgoings(string search, int? i)
         {
             var expensesHistory = _context.Expenses
                 .Include(g => g.Category)
-                .Include(g => g.User).ToList().ToPagedList(i ?? 1,10);
+                .Include(g => g.User).Where(x => x.Category.Name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1,10);
 
 
             return View(expensesHistory);
