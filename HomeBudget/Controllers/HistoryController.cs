001ea9481b482e6ae460f65c8555544ef657463f
Paweł <praczykp2705@vp.pl>
2019-04-07 21:03:08 +0200

    Add sorting

--- HomeBudget/Controllers/HistoryController.cs
+++ HomeBudget/Controllers/HistoryController.cs
@@ -32,20 +32,33 @@ namespace HomeBudget.Controllers
         public ActionResult Incomes(string sortOrder, string search, int? i)
         {
             ViewBag.NamesortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.UsersortParm = String.IsNullOrEmpty(sortOrder) ? "user_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
             ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
 
             var incomesHistory = _context.Incomes
                 .Include(g => g.CategoryIncome)
-                .Include(g => g.User).Where(x => x.CategoryIncome.Name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1,10);
+                .Include(g => g.User).Where(x => x.CategoryIncome.Name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1,100);
 
             switch (sortOrder)
             {
                 case "name_desc":
-                    incomesHistory = incomesHistory.OrderBy(g => g.CategoryIncome.Name).ToList().ToPagedList(i ?? 1,10);
+                    incomesHistory = incomesHistory.OrderBy(g => g.CategoryIncome.Name).ToList().ToPagedList(i ?? 1,100);
+                    break;
+                case "user_desc":
+                    incomesHistory = incomesHistory.OrderBy(g => g.User.UserName).ToList().ToPagedList(i ?? 1, 100);
                     break;
                 case "price_desc":
-                    incomesHistory = incomesHistory.OrderBy(g => g.Price).ToList().ToPagedList(i ?? 1, 10);
+                    incomesHistory = incomesHistory.OrderByDescending(g => g.Price).ToList().ToPagedList(i ?? 1,100);
+                    break;
+                case "Price":
+                    incomesHistory = incomesHistory.OrderBy(g => g.Price).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "date_desc":
+                    incomesHistory = incomesHistory.OrderByDescending(g => g.Date).ToList().ToPagedList(i ?? 1,100);
+                    break;
+                case "Date":
+                    incomesHistory = incomesHistory.OrderBy(g => g.Date).ToList().ToPagedList(i ?? 1, 100);
                     break;
             }
 
@@ -53,12 +66,38 @@ namespace HomeBudget.Controllers
         }
 
         [Authorize]
-        public ActionResult Outgoings(string search, int? i)
+        public ActionResult Outgoings(string sortOrder, string search, int? i)
         {
+            ViewBag.NamesortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.UsersortParm = String.IsNullOrEmpty(sortOrder) ? "user_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
+
             var expensesHistory = _context.Expenses
                 .Include(g => g.Category)
                 .Include(g => g.User).Where(x => x.Category.Name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1,10);
 
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    expensesHistory = expensesHistory.OrderBy(g => g.Category.Name).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "user_desc":
+                    expensesHistory = expensesHistory.OrderBy(g => g.User.UserName).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "price_desc":
+                    expensesHistory = expensesHistory.OrderByDescending(g => g.Price).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "Price":
+                    expensesHistory = expensesHistory.OrderBy(g => g.Price).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "date_desc":
+                    expensesHistory = expensesHistory.OrderByDescending(g => g.Date).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+                case "Date":
+                    expensesHistory = expensesHistory.OrderBy(g => g.Date).ToList().ToPagedList(i ?? 1, 100);
+                    break;
+            }
 
             return View(expensesHistory);
         }
