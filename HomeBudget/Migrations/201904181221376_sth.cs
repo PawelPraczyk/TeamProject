915bdb8da9919dc943f7b1eb68feff20584050d6
Weronika <wlisowska7@gmail.com>
2019-04-18 14:24:18 +0200

    h

---
+++ HomeBudget/Migrations/201904181221376_sth.cs
@@ -0,0 +1,40 @@
+namespace HomeBudget.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sth : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.FixedExpenses", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.FixedExpenses", "User_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.FixedExpenses", new[] { "CategoryId" });
+            DropIndex("dbo.FixedExpenses", new[] { "User_Id" });
+            DropColumn("dbo.AspNetUsers", "UserPhoto");
+            DropTable("dbo.FixedExpenses");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.FixedExpenses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Date = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                        User_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.AspNetUsers", "UserPhoto", c => c.Binary());
+            CreateIndex("dbo.FixedExpenses", "User_Id");
+            CreateIndex("dbo.FixedExpenses", "CategoryId");
+            AddForeignKey("dbo.FixedExpenses", "User_Id", "dbo.AspNetUsers", "Id");
+            AddForeignKey("dbo.FixedExpenses", "CategoryId", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+    }
+}
