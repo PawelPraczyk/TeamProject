c0aa732c0b5d90392f5d37312ad486046e45ac73
Janek Cieszczyk <janek.cieszczyk@gmail.com>
2019-05-05 20:47:03 +0200

    podzial login/logout

--- HomeBudget/Controllers/AccountController.cs
+++ HomeBudget/Controllers/AccountController.cs
@@ -407,7 +407,7 @@ namespace HomeBudget.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "beforeLogin");
         }
 
         //
--- HomeBudget/Controllers/HomeController.cs
+++ HomeBudget/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 
 namespace HomeBudget.Controllers
