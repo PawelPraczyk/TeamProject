61351da1e646d49882ec5ac41fcb671a9aed399d
Weronika <wlisowska7@gmail.com>
2019-05-27 22:20:51 +0200

    dodanie wyswietlanie listy ikonek przy dodawaniu kategorii

--- HomeBudget/Migrations/201905270952520_hmm.cs
+++ HomeBudget/Migrations/201905270952520_hmm.cs
@@ -3,16 +3,14 @@ namespace HomeBudget.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class PopulateCategoriesTable : DbMigration
+    public partial class hmm : DbMigration
     {
         public override void Up()
         {
-            
         }
-
+        
         public override void Down()
         {
-            Sql("DELETE FROM CATEGORIES WHERE Id IN (1,2,3,4,5)");
         }
     }
 }
--- HomeBudget/Models/ApplicationDbContext.cs
+++ HomeBudget/Models/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace HomeBudget.Models
         public DbSet<CategoryIncome> CategoryIncomes { get; set; }
         public DbSet<FixedExpense> FixedExpenses { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<CatColor> Colors { get; set; }
+        public DbSet<MyImage> Icons { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
